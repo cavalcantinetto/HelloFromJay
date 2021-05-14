9e5ff5ac91d1298205db4a8e5676d87dd1803567
jcavalcanti <josecavalcanti@Joses-MacBook-Pro.local>
2021-05-14 06:40:56 -0300

    Strech challenge. This is a bad commit to be reverteed

--- HelloFromJay/src/cavalcanti/Main.java
+++ HelloFromJay/src/cavalcanti/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jose Cavalcanti!");
         System.out.println("Hello,Jose Cavalcanti, my name is Moroni!");
+        System.out.println("Hello,Jose Cavalcanti, This is only to make the strech assignment");
     }
 }
---
+++ WeatherCondition/src/cavalcantinetto/WeatherConditions.java
@@ -0,0 +1,2 @@
+package cavalcantinetto;public class WeatherConditions {
+}
